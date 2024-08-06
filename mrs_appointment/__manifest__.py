fbeb6d6a1342135f056004ca414eebb9d1f41f77
Lwin Maung Maung <lwinmaungmaung.ytu@gmail.com>
2024-08-06 15:57:49 +0630

    Fixes button orders

---
+++ mrs_appointment/__manifest__.py
@@ -0,0 +1,19 @@
+# pylint: disable=pointless-statement,missing-module-docstring,duplicate-code
+{
+    "name": "Medical Record System Appointment",
+    "version": "1.0.0",
+    "category": "medical",
+    "summary": "Medical Record System Appointment",
+    "description": "Medical Record System Appointment",
+    "sequence": "1",
+    "website": "https://www.cosmosmm.com",
+    "author": "Lwin Maung Maung",
+    "maintainer": "lwinmaungmaung@cosmosmm.com",
+    "license": "LGPL-3",
+    "support": "lwinmaungmaung@cosmosmm.com",
+    "depends": ["mrs_mrs", "appointment"],
+    "demo": [],
+    "data": [],
+    "application": True,
+    "installable": True,
+}
